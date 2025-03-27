6e8bc29bd3c75e0af094c21c498345ec5bb699bb
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-03-27 23:27:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (72.33%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -12,24 +12,43 @@
 class Solution {
 public:
     
-vector<int> preorderTraversal(TreeNode* root) {
-    if(!root) return {};
-    vector<int> ans;
-    stack<TreeNode*> st;
-    st.push(root);
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        TreeNode* curr = root;
 
-    while(!st.empty()){
-        TreeNode* node = st.top();
-        st.pop();
-        ans.push_back(node->val);
+        while(curr){
+            //CASE1: No leftchild
+            //Visit node, move right
+            if(!curr->left){
+                res.push_back(curr->val);
+                curr = curr->right;
+            }else{
+                //find predecessor
+                //(rightmost node in left subtree)
+                TreeNode* pred = curr->left;
+                while(pred-> right && pred-> right != curr){
+                    pred = pred->right;
+                }
 
-        if (node->right) st.push(node->right);  
-        if (node->left) st.push(node->left);
+                //create thread (first visit)
+                if(!pred->right){
+                    pred->right = curr; //threading
+                    res.push_back(curr->val);
+                    curr = curr->left;
+                }else{
+                    //break thread(second visit)
+                    pred->right = nullptr; //remove thread
+                    curr = curr->right;
+                }
+            }
+        }
+
+        return res;
+
+
+        
+
     }
-
-    return ans;
-
-}
 
 
 
