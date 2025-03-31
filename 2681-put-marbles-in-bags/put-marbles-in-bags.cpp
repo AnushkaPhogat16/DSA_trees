64e81a05998d99361efc9fba95c56f90d9b83d90
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-03-31 11:43:23 +0530

    Time: 41 ms (32.49%) | Memory: 64.4 MB (76.79%) - LeetSync

--- 2681-put-marbles-in-bags/put-marbles-in-bags.cpp
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     long long putMarbles(vector<int>& weights, int k) {
-        vector<int> pairSum;
+        int n = weights.size();
+        if (k == 1) return 0;  // No pairs to consider, return 0
 
-        //for k, we will have k - 1 pairs
+        vector<int> pairSum(n - 1);
 
-        for(int i = 0; i < weights.size() - 1; i++){
-            pairSum.push_back(weights[i] + weights[i + 1]);
+        // Compute adjacent pair sums directly in the vector
+        for (int i = 0; i < n - 1; i++) {
+            pairSum[i] = weights[i] + weights[i + 1];
         }
 
+        // Sort the entire array
         sort(pairSum.begin(), pairSum.end());
 
-        long long minSum = 0;
-        long long maxSum =0 ;
-
-        for(int i = 0; i < k - 1; i++){
+        long long minSum = 0, maxSum = 0;
+        
+        // Sum of k-1 smallest and k-1 largest elements
+        for (int i = 0; i < k - 1; i++) {
             minSum += pairSum[i];
-        }
-
-        int n = pairSum.size();
-
-        for(int i = 0; i < k - 1; i++){
-            maxSum += pairSum[n - i - 1];
+            maxSum += pairSum[n - 2 - i];  // n-2 because pairSum has size (n-1)
         }
 
         return maxSum - minSum;
