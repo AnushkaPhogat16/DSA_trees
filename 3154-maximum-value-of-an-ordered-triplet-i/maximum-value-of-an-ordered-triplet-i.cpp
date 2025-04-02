88e02a21ef9c213fb0999eaf215723d574484107
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-04-02 10:57:40 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (17.47%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
+        if(nums.size() < 3) return 0;
+        int n = nums.size();
         long long maxVal = 0;
 
-        for(int i = 0; i < nums.size(); i++){
-            for(int j = i + 1; j < nums.size(); j++){
-                for(int z = j+1; z < nums.size(); z++){
+        int maxLeft = nums[0];
+        vector<int> suffMax(n, 0);
+        suffMax[n - 1] = nums[n - 1];
 
-                    long long curr = (long long)(nums[i] - nums[j]) * nums[z];
+        for(int k = n - 2; k >= 0; k--){
+            suffMax[k] = max(nums[k], suffMax[k + 1]);
+        }
 
-                    
-                    maxVal = max(maxVal, curr );
-                }
+        for(int j = 1; j < n - 1; j++){
+            if(nums[j] < maxLeft){
+                long long eq = (long long)(maxLeft - nums[j]) * suffMax[j + 1];
+                maxVal = max(maxVal, eq);
             }
+
+            maxLeft = max(maxLeft, nums[j]);
         }
 
         return maxVal;
