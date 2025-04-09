1a257a9e814b0adf1e000ec07338e36e04e62b41
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-04-09 12:27:36 +0530

    Time: 3 ms (99.06%) | Memory: 31.1 MB (92.14%) - LeetSync

--- 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.cpp
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        //return the number of distinct integers in the array that are greater than k.
-        unordered_set<int> avail;
-        int minOp = 0;
-        for(int i = 0; i < nums.size(); i++){
-            avail.insert(nums[i]);
+        sort(nums.begin(),nums.end()); 
+        if(nums[0]<k){return -1;}
+        int operation = 0;
+        for(int i=nums.size()-1;i>=1;--i){
+            if(nums[i]!=nums[i-1]){
+                ++operation;
+            }
         }
-
-        for(auto& el : avail){
-            if(el > k) minOp++;
-            if(el < k) return -1;
-        }
-
-       if (avail.size() == 1 && *avail.begin() == k){
-            return 0;
-        }
-
-        return minOp;
-        
+        if(nums[0]>k){operation++;} 
+        return operation;
     }
 };
