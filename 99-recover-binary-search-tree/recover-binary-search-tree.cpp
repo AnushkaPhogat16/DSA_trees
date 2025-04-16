ae18454e20c9547caa75055bcee2afc720140e80
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-04-16 23:21:52 +0530

    Time: 0 ms (100.00%) | Memory: 60.9 MB (59.29%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -1,44 +1,41 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-private:
-    TreeNode* first;
-    TreeNode* prev;
-    TreeNode* mid;
-    TreeNode* last;
 public:
-    void inorderTrav(TreeNode* root){
-        if(!root) return;
+    void recoverTree(TreeNode* root) {
+        TreeNode *first = nullptr, *second = nullptr, *prev = nullptr;
+        TreeNode *curr = root;
 
-        inorderTrav(root->left);
+        while (curr) {
+            if (curr->left == nullptr) {
+                // Detect swapped nodes
+                if (prev && curr->val < prev->val) {
+                    if (!first) first = prev;
+                    second = curr;
+                }
+                prev = curr;
+                curr = curr->right;
+            } else {
+                TreeNode* pred = curr->left;
+                while (pred->right && pred->right != curr) {
+                    pred = pred->right;
+                }
 
-        if(prev != NULL && (root->val < prev->val)){
-            if(first == NULL){
-                first = prev;
-                mid = root;
-            }else{
-                last = root;
+                if (pred->right == nullptr) {
+                    pred->right = curr;
+                    curr = curr->left;
+                } else {
+                    pred->right = nullptr;
+
+                    // Detect swapped nodes
+                    if (prev && curr->val < prev->val) {
+                        if (!first) first = prev;
+                        second = curr;
+                    }
+                    prev = curr;
+                    curr = curr->right;
+                }
             }
         }
 
-        prev = root;
-        inorderTrav(root->right);
-    }
-    void recoverTree(TreeNode* root) {
-        first = mid = last = NULL;
-        prev = new TreeNode(INT_MIN);
-        inorderTrav(root);
-
-        if(first && last) swap(first->val , last->val);
-        else if(first && mid) swap(first->val, mid->val);
+        if (first && second) swap(first->val, second->val);
     }
 };
