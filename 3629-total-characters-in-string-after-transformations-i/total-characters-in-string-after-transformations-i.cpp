d1288710ebc6c9999f5f8542cb16ce4c75f8a22e
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-05-13 13:43:19 +0530

    Time: 78 ms (82.27%) | Memory: 21.1 MB (63.55%) - LeetSync

--- 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.cpp
+++ 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.cpp
@@ -1,39 +1,45 @@
 class Solution {
 public:
-    const int MOD = 1e9 + 7;
-
-    int lengthAfterTransformations(string s, int t) {
-        vector<int> mapp(26, 0);
-
-        // Initialize the frequency map
-        for (char &ch : s) {
-            mapp[ch - 'a']++;
+    const long long MOD = 1e9 + 7;
+    int f1(string s, int t){
+        vector<int> freq(26,0);
+        int n = s.size();
+        for(int i=0; i<n; i++){
+            freq[s[i]-'a']++;
         }
-
-        while (t > 0) {
-            vector<int> temp(26, 0);
-
-            for (int i = 0; i < 26; i++) {
-                char letter = i + 'a';
-                int freq = mapp[i];
-
-                if (letter != 'z') {
-                    temp[(letter + 1) - 'a'] = (temp[(letter + 1) - 'a'] + freq) % MOD;
-                } else {
-                    temp[0] = (temp[0] + freq) % MOD;
-                    temp[1] = (temp[1] + freq) % MOD;
-                }
+        while(t--){
+            int last = freq[25];
+            for(int i=25; i>0; i--){
+                freq[i] = freq[i-1];
             }
-            mapp = move(temp);
-            t--;
+            freq[1] = (freq[1] + last)%MOD;
+            freq[0] = last;
         }
-
-        // Calculate the total length
-        long long ans = 0;
-        for (int i = 0; i < 26; i++) {
-            ans = (ans + mapp[i]) % MOD;
+        int sum = 0;
+        for(int i=0; i<26; i++){
+            sum = (sum + freq[i])%MOD;
         }
-
-        return ans;
+        return sum;
+    }
+    int f(int ch, int t){
+        if(t < (26 - ch)) return 1;
+        // 26 - ch gives the number of transformations for doubling
+        return (f(0, t-26 + ch)%MOD + f(1, t-26 + ch)%MOD)%MOD;
+    }
+    int lengthAfterTransformations(string s, int t) {
+        // int val = 0;
+        // vector<int> freq(26, 0);
+        // vector<int> dp(26, -1);
+        // int n = s.size();
+        // for(int i=0; i<n; i++){
+        //     freq[s[i] - 'a']++;
+        // }
+        // for(int i=0; i<26; i++){
+        //     if(dp[i] == -1){
+        //         dp[i] = f(i, t)%MOD;
+        //     }
+        //     val = (val + (dp[i]*freq[i])%MOD)%MOD;
+        // }
+        return f1(s, t);
     }
 };
