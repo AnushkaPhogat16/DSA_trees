5a3d5894de665cac53304189cdb9bf7e9bde56ee
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-06-07 19:42:13 +0530

    Time: 47 ms (70.82%) | Memory: 10.1 MB (96.43%) - LeetSync

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
+    int bestStartingPoint(string &word, int n){
+        int i = 0;
+        int j = 1;
+
+        while(j < n){
+            int k = 0;
+            while(j + k < n &&  word[i + k] == word[j + k]){
+                k++;
+            }
+
+            if(j + k < n && word[j+k] > word[i+k]){
+                i = j;
+                j++;
+            }else{
+                j = j + k + 1;
+
+            }
+        }
+
+        return i;
+
+
+    }
     string answerString(string word, int numFriends) {
         int n = word.size();
         if(numFriends == 1) return word;
-        
-        string result;
+
+        int index = bestStartingPoint(word, n);
 
         int maxLength = n - numFriends + 1;
+        int possibleLen = min(maxLength, (n - index));
 
-        for(int i = 0; i < n; i++){
-            int possibleLen = min(maxLength, n - i);
-            result = max(result, word.substr(i, possibleLen));
-        }
+        
 
-        return result;        
+        return word.substr(index, possibleLen);        
     }
 
 };
