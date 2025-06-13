dab33ae8535da6f6e97641a3d43eb2defd1f4d92
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-06-13 18:01:02 +0530

    Time: 0 ms (100.00%) | Memory: 26.9 MB (44.30%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int left = 0, right = height.size() - 1;
-        int lmax = 0, rmax = 0;
+        stack<int> st;
         int totalWater = 0;
 
-        while (left <= right) {
-            if (height[left] <= height[right]) {
-                if (height[left] >= lmax) {
-                    lmax = height[left];
-                } else {
-                    totalWater += lmax - height[left];
-                }
-                left++;
-            } else {
-                if (height[right] >= rmax) {
-                    rmax = height[right];
-                } else {
-                    totalWater += rmax - height[right];
-                }
-                right--;
+        for (int i = 0; i < height.size(); i++) {
+            while (!st.empty() && height[i] > height[st.top()]) {
+                int bottomIdx = st.top();
+                st.pop();
+
+                if (st.empty())
+                    break;
+
+                int leftIdx = st.top();
+                int distance = i - leftIdx - 1;
+                int boundedHeight = min(height[i], height[leftIdx]) - height[bottomIdx];
+
+                totalWater += distance * boundedHeight;
             }
+            st.push(i);
         }
 
         return totalWater;
