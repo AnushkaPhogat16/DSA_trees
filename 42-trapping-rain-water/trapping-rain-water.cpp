8acdd6608c03d987bc0642d33ef34bd82715500e
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-06-13 17:15:53 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (79.74%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
-    int trap(vector<int>& h) {
-        vector<int> lmax(h.size());
-        vector<int> rmax(h.size());
-
-        lmax[0] = h[0];
-        rmax[h.size() - 1] = h[h.size() - 1];
-
-        int lmaxVal = h[0];
-        for(int i = 1; i < h.size(); i++){
-            lmaxVal = max(lmaxVal , h[i]);
-
-            lmax[i] = lmaxVal;
-        }
-
-        int rmaxVal = h[h.size() - 1];
-        for(int i = h.size() - 2; i >= 0; i--){
-            rmaxVal = max(rmaxVal , h[i]);
-
-            rmax[i] = rmaxVal;
-        }
-
+    int trap(vector<int>& height) {
+        int left = 0, right = height.size() - 1;
+        int lmax = 0, rmax = 0;
         int totalWater = 0;
 
-        for(int i = 0; i < h.size(); i++){
-            // min(hmax[i], rmax[i]) - h[i];
-
-            int height = min(rmax[i] , lmax[i]) - h[i];
-            totalWater += height;
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= lmax) {
+                    lmax = height[left];
+                } else {
+                    totalWater += lmax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rmax) {
+                    rmax = height[right];
+                } else {
+                    totalWater += rmax - height[right];
+                }
+                right--;
+            }
         }
 
         return totalWater;
     }
 };
-
-//      4 5 0 3 2 7
-// lmax 4 5 5 5 5 7
-// rmax 7 7 7 7 7 7
