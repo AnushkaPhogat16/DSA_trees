08da388ff8c54bb3862d31647ac04fd8a27fe979
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-06-15 12:13:07 +0530

    Time: 104 ms (32.72%) | Memory: 182.2 MB (36.94%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,11 +1,7 @@
 class LRUCache {
 public:
-    list<int> dll; //key
-    // doubly linked list
-     
+    list<int> dll;
     unordered_map<int, pair<list<int>::iterator, int>> mp;
-    // address type in linked list
-
     int n;
 
     LRUCache(int capacity) {
@@ -19,43 +15,23 @@ public:
     }
     
     int get(int key) {
-        if(mp.find(key) == mp.end()){
-            // key does not exist
-            return -1;
-        }
-
-        //have to mark as recently used
+        if(mp.find(key) == mp.end()) return -1;
         makeRecentlyUsed(key);
-
         return mp[key].second;
-
-
     }
     
     void put(int key, int value) {
         if(mp.find(key) != mp.end()){
             mp[key].second = value;
             makeRecentlyUsed(key);
-        }else{
+        } else {
+            if(dll.size() == n){
+                int keyToDelete = dll.back();
+                mp.erase(keyToDelete);
+                dll.pop_back();
+            }
             dll.push_front(key);
-            mp[key] = {dll.begin() , value};
-            n--;
+            mp[key] = {dll.begin(), value};
         }
-
-        if(n < 0){
-            int keyToDelete = dll.back();
-            mp.erase(keyToDelete);
-
-            dll.pop_back();
-            n++;
-        }
-        return;
     }
 };
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
