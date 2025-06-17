d6d89e85dba863fb61a7461eaf4e625038b3917e
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-06-17 14:17:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (13.69%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        long long N = n;  // Use long long to handle INT_MIN safely
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
-        }
-        return helper(x, N);
+        return solve(x, (long)n);
     }
 
-    double helper(double x, long long n) {
-        if (n == 0) return 1;
-        double half = helper(x, n / 2);
-        if (n % 2 == 0) {
-            return half * half;
-        } else {
-            return half * half * x;
+    double solve(double x, long n){
+        if(n == 0) return 1;
+        if(n < 0) return solve(1/x , -n);
+
+        if(n % 2 == 0){
+            return solve(x*x , n/2);
+        }else{
+            return x * solve(x*x , (n-1)/2);
         }
-    }
+        
+    } 
 };
