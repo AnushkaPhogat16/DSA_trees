f25519f087d8faec6c19e21efabd1312ccb596aa
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-06-24 13:24:06 +0530

    Time: 7 ms (51.93%) | Memory: 14.5 MB (90.46%) - LeetSync

--- 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.cpp
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
     vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
-        vector<int> keyInds;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] == key){
-                keyInds.push_back(i);
+        vector<int> res;
+        int n = nums.size();
+        // traverse number pairs
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (nums[j] == key && abs(i - j) <= k) {
+                    res.push_back(i);
+                    break;  // early termination to prevent duplicate addition
+                }
             }
         }
-
-        vector<int> result;
-
-        // [2, 5]
-
-        for(int i = 0; i < nums.size(); i++){
-            int minDistFromKey = INT_MAX;
-            for(int ind : keyInds){
-                minDistFromKey = min(minDistFromKey, abs(i - ind));
-            }
-            if(minDistFromKey <= k){
-                result.push_back(i);
-            }
-        }
-
-        return result;
-
+        return res;
     }
 };
