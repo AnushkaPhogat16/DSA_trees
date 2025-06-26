d51d494d6316ee7789d39e3e7433cccd0d4b08d8
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-06-27 03:36:54 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (72.49%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
-    int t[101][101];
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        
         int rows = obstacleGrid.size();
         int cols = obstacleGrid[0].size();
 
-        if(obstacleGrid[rows - 1][cols - 1] == 1) return 0;
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[rows - 1][cols - 1] == 1)
+            return 0;
 
-        memset(t, -1, sizeof(t));
+        vector<vector<int>> dp(rows, vector<int>(cols, 0));
 
-        return solve(rows - 1, cols - 1, obstacleGrid);
-    }
-
-    int solve(int ind1, int ind2, vector<vector<int>>& obstacleGrid){
-        if(t[ind1][ind2] != -1) return t[ind1][ind2];
-
-        if(ind1 == 0 && ind2 == 0) return 1;
-        if(ind1 < 0 || ind2 < 0) return 0;
-
-        int up = 0;
-        if(ind1 > 0 && obstacleGrid[ind1 - 1][ind2] != 1 ){
-            up = solve(ind1 - 1, ind2, obstacleGrid);
+        // Fixing first column initialization
+        for (int i = 0; i < rows; i++) {
+            if (obstacleGrid[i][0] == 1) break;
+            dp[i][0] = 1;
         }
 
-        int left = 0;
-        if(ind2 > 0 && obstacleGrid[ind1][ind2 - 1] != 1 ){
-            left = solve(ind1, ind2- 1, obstacleGrid);
+        // Fixing first row initialization
+        for (int j = 0; j < cols; j++) {
+            if (obstacleGrid[0][j] == 1) break;
+            dp[0][j] = 1;
         }
 
-        return t[ind1][ind2] = up + left;
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[rows - 1][cols - 1];
     }
 };
