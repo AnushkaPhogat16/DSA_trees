5689fa09b49ab77b204ee012bb6a8e5779c77502
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-06-27 02:39:43 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (8.90%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    int t[101][101];
     int uniquePaths(int m, int n) {
-        memset(t, -1, sizeof(t));
-        return solve(0, 0, m , n);
-    }
+        vector<vector<int>> dp(m, vector<int>(n, 0));
 
-    int solve(int ind1, int ind2, int m, int n){
-        if(ind1 == m - 1 && ind2 == n - 1){
-            return 1;
+        for(int i = 0; i < m; i++) dp[i][0] = 1;
+        for(int j = 0; j < n; j++) dp[0][j] = 1;
+
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
         }
 
-        if(ind1 >= m || ind2 >= n){
-            return 0;
-        }
-
-        if(t[ind1][ind2] != -1) return t[ind1][ind2];
-
-        return t[ind1][ind2] = (solve(ind1 + 1, ind2, m , n) + solve(ind1, ind2+1, m, n));
+        return dp[m-1][n-1];
     }
 };
