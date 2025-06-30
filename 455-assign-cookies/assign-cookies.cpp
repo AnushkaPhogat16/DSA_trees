df5bcadaeacc32c1f46caa29740ebce1cb56f1ff
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-06-30 23:54:31 +0530

    Time: 5 ms (82.19%) | Memory: 44.8 MB (43.38%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int number= 0;
-        int indS=0;
-        int indG=0;
-
-        if(s.size() == 0 ){
-            return 0;
-        }
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
-
-        while(indG < g.size()){
-            if(s[indS] >= g[indG]){
-                number++;
-                indS++;
-                indG++;
-            }else{
-                indS++;
+        
+        int child = 0, cookie = 0;
+        
+        while (child < g.size() && cookie < s.size()) {
+            if (s[cookie] >= g[child]) {
+                child++;  // satisfied
             }
-
-            if(indS == s.size()){
-                return number;
-            }
+            cookie++;  // move to next cookie
         }
-
-        return number;
+        
+        return child;  // number of satisfied children
     }
+
 };
