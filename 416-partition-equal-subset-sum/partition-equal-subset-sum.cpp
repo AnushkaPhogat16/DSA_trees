db8cd13f19465fecef89a4e66b9614685ce34976
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-06-30 21:08:59 +0530

    Time: 179 ms (18.20%) | Memory: 25.3 MB (49.35%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -5,22 +5,27 @@ public:
         int total = accumulate(nums.begin(), nums.end(), 0);
         if (total % 2 != 0) return false;
         int target = total / 2;
-        memset(t, -1, sizeof(t));
-        return f(nums.size() - 1, target, nums);
-    }
 
-    bool f(int ind, int tgt, vector<int>& nums){
-        if(tgt == 0) return true;
-        if(ind  == 0) return nums[0] == tgt;
+        int n = nums.size();
 
-        if(t[ind][tgt] != -1) return t[ind][tgt];
+        vector<vector<bool>> dp(n, vector<bool>(target + 1, false));
 
-        bool notTake = f(ind - 1, tgt, nums);
-        bool take = false;
-        if(nums[ind] <= tgt){
-            take = f(ind-1, tgt-nums[ind], nums);
+        //base case
+        for(int i = 0; i < n; i++) dp[i][0] = true;
+        if(nums[0] <= target) dp[0][nums[0]] = true;
+
+        for(int ind = 1; ind < n; ind++){
+            for(int t = 1; t <= target; t++){
+                bool notTake = dp[ind-1][t];
+                bool take = false;
+                if(nums[ind] <= t){
+                    take = dp[ind-1][t-nums[ind]];
+                }
+                dp[ind][t] = take || notTake;
+            }
         }
 
-        return t[ind][tgt] = take || notTake;
+        return dp[n-1][target];
+
     }
 };
