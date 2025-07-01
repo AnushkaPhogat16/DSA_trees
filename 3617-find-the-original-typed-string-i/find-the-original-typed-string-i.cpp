5e395121de98087083a166ab344837601d8ae247
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-07-01 08:34:56 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (29.69%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int possibleStringCount(string word) {
+        if (word.empty()) return 0; 
+
         int ans = 0;
         char prev = word[0];
-        for(int i = 1; i < word.length(); i++){
-            if(word[i] == prev){
+
+        for (int i = 1; i < word.length(); i++) {
+            if (word[i] == prev) {
                 ans++;
-            }else{
+            } else {
                 prev = word[i];
             }
-
         }
 
         return ans + 1;
