41b0823fd858095e6b6239946f1b8d0e585ff540
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-07-01 23:37:10 +0530

    Time: 30 ms (59.04%) | Memory: 44.3 MB (22.30%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    int f(int i, int j, string& s, string& t){
-        if(j < 0) return 1;
-        if(i < 0) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-
-        if(s[i] == t[j]){
-            return dp[i][j] = f(i - 1, j - 1, s, t) + f(i - 1, j, s, t);
-        }
-
-        return dp[i][j] = f(i - 1, j, s, t); 
-
-    }
-
     int numDistinct(string s, string t) {
         int n = s.length();
         int m = t.length();
 
-        memset(dp, -1, sizeof(dp));
+        vector<vector<unsigned long long>> dp(n + 1, vector<unsigned long long>(m + 1, 0));
 
-        return f(n - 1, m - 1, s, t);
-        
+        for (int i = 0; i <= n; i++) dp[i][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][m];  // auto-cast to int, safe since output is expected to be int
     }
 };
