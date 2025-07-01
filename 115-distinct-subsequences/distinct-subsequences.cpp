78830e28a86d755acc978667e7df0d861d2f2e5e
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-07-01 23:39:50 +0530

    Time: 10 ms (90.74%) | Memory: 9.1 MB (82.46%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -4,20 +4,22 @@ public:
         int n = s.length();
         int m = t.length();
 
-        vector<vector<unsigned long long>> dp(n + 1, vector<unsigned long long>(m + 1, 0));
+        vector<unsigned long long> prev(m + 1, 0), curr(m + 1, 0);
 
-        for (int i = 0; i <= n; i++) dp[i][0] = 1;
+        prev[0] = 1;  
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
+            curr[0] = 1;  
+            for (int j = m; j >= 1; j--) {  
                 if (s[i - 1] == t[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                    curr[j] = prev[j - 1] + prev[j];
                 } else {
-                    dp[i][j] = dp[i - 1][j];
+                    curr[j] = prev[j];
                 }
             }
+            prev = curr;
         }
 
-        return dp[n][m];  // auto-cast to int, safe since output is expected to be int
+        return static_cast<int>(prev[m]);
     }
 };
