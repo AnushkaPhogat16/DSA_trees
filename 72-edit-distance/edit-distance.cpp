1ebdae5c94e5b05a9ae21e25d040d9be782be0cb
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-07-02 00:53:49 +0530

    Time: 10 ms (34.66%) | Memory: 13.1 MB (61.95%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    int f(int i, int j, string& s1, string& s2, vector<vector<int>>& dp) {
-        if (i < 0) return j + 1;
-        if (j < 0) return i + 1;
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        if (s1[i] == s2[j]) {
-            return dp[i][j] = f(i - 1, j - 1, s1, s2, dp);
-        }
-
-        return dp[i][j] = 1 + min({
-            f(i - 1, j, s1, s2, dp),     // delete
-            f(i, j - 1, s1, s2, dp),     // insert
-            f(i - 1, j - 1, s1, s2, dp)  // replace
-        });
-    }
-
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
 
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return f(n - 1, m - 1, word1, word2, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        // Base cases
+        for (int i = 0; i <= n; i++) dp[i][0] = i; // converting word1[0..i-1] to empty = i deletions
+        for (int j = 0; j <= m; j++) dp[0][j] = j; // converting empty to word2[0..j-1] = j insertions
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1]; // no operation needed
+                } else {
+                    dp[i][j] = 1 + min({
+                        dp[i - 1][j],     // delete
+                        dp[i][j - 1],     // insert
+                        dp[i - 1][j - 1]  // replace
+                    });
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 };
