69a1f58890af9f90aa22f773be92acf5d94e0682
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-07-16 09:02:46 +0530

    Time: 4 ms (12.09%) | Memory: 8.7 MB (96.89%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,41 +1,19 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        // find start nd end index of every primitive
-        // remove 1st nd last bracket
-        // add it to string
-
-        vector<pair<int, int>> startEndInd;
-        stack<int> st;
-
-        int start = 0;
-
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] == '('){
-                st.push(s[i]);
-            }else{
-                st.pop();
-            }
-
-            if(st.empty()){
-                startEndInd.push_back({start, i});
-                start = i + 1;
-            }
-        }
-
         string ans = "";
+        int depth = 0;
 
-        for(auto &i : startEndInd){
-            int strt = i.first;
-            int end = i.second;
-
-            for(int k = strt+1; k < end; k++){
-                ans += s[k];
+        for (char c : s) {
+            if (c == '(') {
+                if (depth > 0) ans += c;  // skip the outermost '('
+                depth++;
+            } else {
+                depth--;
+                if (depth > 0) ans += c;  // skip the outermost ')'
             }
         }
 
         return ans;
-
-
     }
 };
