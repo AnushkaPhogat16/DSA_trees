a4dc57bf0979ffee2d013b846359553d2bb676a3
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-08-16 10:04:11 +0530

    Time: 70 ms (50.44%) | Memory: 47.9 MB (18.88%) - LeetSync

--- 1332-count-vowels-permutation/count-vowels-permutation.cpp
+++ 1332-count-vowels-permutation/count-vowels-permutation.cpp
@@ -1,52 +1,24 @@
 class Solution {
 public:
-    vector<char> v{'a', 'e', 'i', 'o', 'u'};
     const int MOD = 1e9+7;
-    vector<vector<long long>> dp;
 
     int countVowelPermutation(int n) {
-        dp.assign(n+1, vector<long long>(6, -1)); 
-        return solve(0, n, '#');
-    }
+        // dp[i][c] = number of strings of length i ending with vowel c
+        vector<vector<long long>> dp(n+1, vector<long long>(5, 0));
 
-    int solve(int i, int l, char prev){
-        if(i == l) return 1;
+        // Base case: length 1, each vowel counts as 1
+        for(int j=0; j<5; j++) dp[1][j] = 1;
 
-        int idx = getIndex(prev);
-        if(dp[i][idx] != -1) return dp[i][idx];
-
-        long long ans = 0;
-
-        if(prev == '#'){
-            for(char ch : v){
-                ans = (ans + solve(i+1, l, ch)) % MOD;
-            }
-        } else if(prev == 'a'){
-            ans = (ans + solve(i+1, l, 'e')) % MOD;
-        } else if(prev == 'e'){
-            ans = (ans + solve(i+1, l, 'a')) % MOD;
-            ans = (ans + solve(i+1, l, 'i')) % MOD;
-        } else if(prev == 'i'){
-            for(char ch : v){
-                if(ch == 'i') continue;
-                ans = (ans + solve(i+1, l, ch)) % MOD;
-            }
-        } else if(prev == 'o'){
-            ans = (ans + solve(i+1, l, 'i')) % MOD;
-            ans = (ans + solve(i+1, l, 'u')) % MOD;
-        } else { // 'u'
-            ans = (ans + solve(i+1, l, 'a')) % MOD;
+        for(int i=2; i<=n; i++) {
+            dp[i][0] = (dp[i-1][1] + dp[i-1][2] + dp[i-1][4]) % MOD; // a <- e,i,u
+            dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % MOD;              // e <- a,i
+            dp[i][2] = (dp[i-1][1] + dp[i-1][3]) % MOD;              // i <- e,o
+            dp[i][3] = dp[i-1][2] % MOD;                             // o <- i
+            dp[i][4] = (dp[i-1][2] + dp[i-1][3]) % MOD;              // u <- i,o
         }
 
-        return dp[i][idx] = ans;
-    }
-
-    int getIndex(char c){
-        if(c == 'a') return 0;
-        if(c == 'e') return 1;
-        if(c == 'i') return 2;
-        if(c == 'o') return 3;
-        if(c == 'u') return 4;
-        return 5; // '#'
+        long long ans = 0;
+        for(int j=0; j<5; j++) ans = (ans + dp[n][j]) % MOD;
+        return ans;
     }
 };
