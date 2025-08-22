4463b07a254c3a3dd4064e1392304c63941203ce
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-08-22 10:53:35 +0530

    Time: 267 ms (32.27%) | Memory: 133.4 MB (72.52%) - LeetSync

--- 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.cpp
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -1,52 +1,24 @@
 class Solution {
 public:
     int minimumArea(vector<vector<int>>& grid) {
-        int firstRow = -1, lastRow = -1;
-        int firstCol = -1, lastCol = -1;
+        int m = grid.size();
+        int n = grid[0].size();
 
-        // first row
-        for(int i = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid[0].size(); j++){
-                if(grid[i][j] == 1){
-                    firstRow = i;
-                    goto foundFirstRow;
+        int firstRow = m, lastRow = -1;
+        int firstCol = n, lastCol = -1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    firstRow = min(firstRow, i);
+                    lastRow  = max(lastRow, i);
+                    firstCol = min(firstCol, j);
+                    lastCol  = max(lastCol, j);
                 }
             }
         }
-        foundFirstRow:;
 
-        // first col
-        for(int j = 0; j < grid[0].size(); j++){
-            for(int i = 0; i < grid.size(); i++){
-                if(grid[i][j] == 1){
-                    firstCol = j;
-                    goto foundFirstCol;
-                }
-            }
-        }
-        foundFirstCol:;
-
-        // last row
-        for(int i = grid.size()-1; i >= 0; i--){
-            for(int j = grid[0].size()-1; j >= 0; j--){
-                if(grid[i][j] == 1){
-                    lastRow = i;
-                    goto foundLastRow;
-                }
-            }
-        }
-        foundLastRow:;
-
-        // last col
-        for(int j = grid[0].size()-1; j >= 0; j--){
-            for(int i = grid.size()-1; i >= 0; i--){
-                if(grid[i][j] == 1){
-                    lastCol = j;
-                    goto foundLastCol;
-                }
-            }
-        }
-        foundLastCol:;
+        if (lastRow == -1) return 0; // no '1' in grid
 
         return (lastRow - firstRow + 1) * (lastCol - firstCol + 1);
     }
