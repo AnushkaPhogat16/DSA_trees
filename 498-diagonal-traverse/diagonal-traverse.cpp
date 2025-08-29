4c4f1c3f26817be1094e7ad4c8b33cb2cac96815
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-08-29 13:56:32 +0530

    Time: 31 ms (14.89%) | Memory: 31.2 MB (11.40%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.cpp
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-    vector<int> findDiagonalOrder(vector<vector<int>>& matrix) {
-        if (matrix.empty() || matrix[0].empty()) return {};
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
 
-        int m = matrix.size(), n = matrix[0].size();
-        vector<int> result(m * n);
-        int row = 0, col = 0;
+        map<int, vector<int>> mp;
+        vector<int> res;
 
-        for (int i = 0; i < m * n; i++) {
-            result[i] = matrix[row][col];
-
-            if ((row + col) % 2 == 0) {
-                if (col == n - 1) row++;
-                else if (row == 0) col++;
-                else { row--; col++; }
-            } else {
-                if (row == m - 1) col++;
-                else if (col == 0) row++;
-                else { row++; col--; }
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++ ){
+                mp[i+j].push_back(mat[i][j]);
             }
         }
 
-        return result;
+        bool flip = true;
+
+        for(auto &it : mp){
+            if(flip){
+                reverse(it.second.begin(), it.second.end());
+            }
+
+            for(int &num: it.second){
+                res.push_back(num);
+            }
+            flip = !flip;
+        }
+
+        return res;
     }
 };
