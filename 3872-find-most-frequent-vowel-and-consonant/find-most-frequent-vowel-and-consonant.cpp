948129ef2993edfecfc35e9c899ca5388ff9b6c1
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-09-17 12:30:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (83.02%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     int maxFreqSum(string s) {
+        int freq[26] = {0};
+        int maxV = 0, maxC = 0;
 
-        int mp[26] = {0};
+        for(char ch : s) {
+            int idx = ch - 'a';
+            int f = ++freq[idx];
 
-        for(char ch : s){
-            mp[ch - 'a']++;
-        }
-
-        int v = 0, c = 0;
-
-        for(int i = 0; i < 26; i++){
-            if(i == 0 || i == 4 || i == 8 || i == 14 || i == 20){
-                v = max(v, mp[i]);
-            }else{
-                c = max(c, mp[i]);
+            if(idx == 0 || idx == 4 || idx == 8 || idx == 14 || idx == 20) {
+                maxV = max(maxV, f); // vowel
+            } else {
+                maxC = max(maxC, f); // consonant
             }
         }
 
-        int sum = v + c;
-
-        return sum;
-        
-        // a e i o  u
-        // 0 4 8 14 20  
+        return maxV + maxC;
     }
 };
