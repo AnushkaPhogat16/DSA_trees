9605710c9973ccf95397cc9674f054a2e7837c88
Anushka Phogat <115017897+AnushkaPhogat16@users.noreply.github.com>
2025-09-27 13:47:51 +0530

    Time: 6 ms (68.27%) | Memory: 11.6 MB (77.33%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if(s.size() == 0) return 0;
-        int l = 0, r = 0;
-        int ans = r - l + 1;
-
-
-
-        unordered_map<char, int> f;
-        f[s[0]]++;
-
+        vector<int> lseen(256, -1);
+        int ans = 0;
+        int l = 0;
         
-        while(r < s.size()-1){
-            r++;
-            f[s[r]]++;
-            while(f[s[r]] > 1){
-                f[s[l]]--;
-                l++;
+        for(int r = 0; r < s.size(); r++){
+            if(lseen[s[r]] >= l){
+                l = lseen[s[r]]+1;
             }
+
+            lseen[s[r]] = r;
 
             ans = max(ans, r - l + 1);
         }
 
         return ans;
-        
+
+
     }
 };
